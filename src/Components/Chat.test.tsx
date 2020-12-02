3751d1a052e106e2f5adc14e4863a564e37c7135
Andrea Mori <wamba85@gmail.com>
2020-12-02 10:58:49 +0100

    basic chat

---
+++ src/Components/Chat.test.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { shallow, ShallowWrapper } from "enzyme";
+import Chat from "./Chat";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faWindowMinimize } from "@fortawesome/free-solid-svg-icons";
+
+describe("Chat", () => {
+  let wrapper: ShallowWrapper;
+
+  beforeEach(() => (wrapper = shallow(<Chat />)));
+
+  // it('should render correctly', () => expect(wrapper).toMatchSnapshot());
+  it("should render a header", () => {
+    expect(wrapper.find(".header").length).toEqual(1);
+  });
+
+  it("should render a <div />", () => {
+    expect(wrapper.find("div").length).toEqual(6);
+  });
+
+  it("renders minimize and close icon Component", () => {
+    expect(wrapper.containsMatchingElement(<FontAwesomeIcon icon={faWindowMinimize} />)).toEqual(true);
+    expect(wrapper.containsMatchingElement(<FontAwesomeIcon icon={faTimes} />)).toEqual(true);
+  });
+
+  it("should write text inside <div />", () => {
+    const input = wrapper.find("input");
+
+    input.simulate("focus");
+    input.simulate("keypress", { key: "Enter", currentTarget: { value: "Hello" } });
+
+    expect(wrapper.find(".messages").first().text()).toEqual("Hello");
+  });
+});
---
+++ src/Components/Chat.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faWindowMinimize } from "@fortawesome/free-solid-svg-icons";
+import styles from "./Chat.module.css";
+
+// import PropTypes from 'prop-types'
+
+const Chat = () => {
+  const [message, setMessage] = useState("");
+  const handlerMessage = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setMessage(e.currentTarget.value);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.chatName}>nome</div>
+        <div className={styles.chatIcons}>
+          <FontAwesomeIcon icon={faWindowMinimize} size="xs" />
+          <FontAwesomeIcon icon={faTimes} size="xs" />
+        </div>
+      </div>
+      <div className={styles.messages}>{message}</div>
+      <div className={styles.message}>
+        <input type="text" name="message" onKeyPress={(e) => handlerMessage(e)} />
+      </div>
+    </div>
+  );
+};
+
+Chat.propTypes = {};
+
+export default Chat;
